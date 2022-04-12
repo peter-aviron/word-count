b37d26ce3221a076faaac7c2b140b80d22826b0f
Peter Mayor <peter@avironsoftware.com>
2022-04-12 17:00:42 -0400

    final word count

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 let textArea = document.querySelector("#text");
 // console.log(textArea.innerHTML);
 let button = document.querySelector("button");
+let outputElement = document.querySelector("#output");
 
 button.addEventListener("click", () => {
   const textValue = textArea.value;
-  const wordsArray = textValue.split(" ");
+  const wordsArray = textValue.trim().split(" ");
   let outputObject = {};
   debugger
   for (let i = 0; i < wordsArray.length; i++) {
     const currentWord = wordsArray[i];
     // if the currentWord is in outputObject, increment the currentWord property by 1
     // else put the currentWord as a key in the outputObject with a value of 1
-    if(outputObject.hasOwnProperty(currentWord)){
-        outputObject[currentWord] = outputObject[currentWord] + 1
+    if (outputObject.hasOwnProperty(currentWord)) {
+      outputObject[currentWord] = outputObject[currentWord] + 1;
     } else {
-        outputObject += currentWord
+      outputObject[currentWord] = 1;
+      //   outputObject = {...outputObject, [currentWord]: 1 };
     }
   }
+  outputElement.innerHTML = JSON.stringify(outputObject);
 });
